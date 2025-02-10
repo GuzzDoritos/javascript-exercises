c57804f2ee392c9e0f252b6654f3061823a442a6
Guzz <gustavo.bragada@hotmail.com>
2025-02-10 02:07:47 -0300

    Finish '02_repeatString' exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num < 0) return newString = 'ERROR';
+    while (num--) newString += string; 
+    return newString;
 };
 
 // Do not edit below this line
