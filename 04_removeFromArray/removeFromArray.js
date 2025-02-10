bdf9ace9ebce77961ac93340d157a2d7021e8588
Guzz <gustavo.bragada@hotmail.com>
2025-02-10 11:52:40 -0300

    Finish '04_removeFromArray' exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let filteredArray = [];
+    next: for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (args[j] === array[i]) { 
+                continue next;
+            };    
+        };                 
+        filteredArray.push(array[i])
+    };
+    return filteredArray;
 };
 
 // Do not edit below this line
