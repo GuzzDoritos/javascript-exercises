70229e26371fd6f599f509f9253b65604956128e
Guzz <gustavo.bragada@hotmail.com>
2025-02-10 12:42:55 -0300

    Finish '05_sumAll' exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    let isDecimal = parseInt(firstNum) !== firstNum || parseInt(lastNum) !== lastNum;
+    let isNegative = firstNum < 0 || lastNum < 0;
+    let isNotNumber = typeof firstNum !== "number" || typeof lastNum !== "number";
+    if (isNegative || isDecimal || isNotNumber) {
+        return 'ERROR';
+    }
+    if (firstNum < lastNum) {
+        for (let i = firstNum; i <= lastNum; i++) {
+            sum += i;
+        }        
+    } else {
+        for (let i = lastNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
