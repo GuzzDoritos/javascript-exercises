c87342fb7e19daa5ff5930047f28bba0e09d89a9
Guzz <gustavo.bragada@hotmail.com>
2025-02-14 18:40:45 -0300

    Finish '09_palindromes' exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    const cleanStr = str.toLowerCase()
+    .split('')
+    .filter(char => alpha.includes(char))
+    .join('');
+
+    const reverseStr = cleanStr.split('').reverse().join('');
+
+    return reverseStr === cleanStr;
 };
 
 // Do not edit below this line
