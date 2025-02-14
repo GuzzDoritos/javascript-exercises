491de7ea950b8fe7b92f2410e768ea8b8faa1c1d
Guzz <gustavo.bragada@hotmail.com>
2025-02-14 18:18:02 -0300

    Finish '08_calculator' exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
+  // original = a;
+  // for (let i = 1; i < b; i++) {
+  //   a *= original;
+  // }
+  // return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  for (let i = num - 1; i > 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
