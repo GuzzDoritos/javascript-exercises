1cb38b32037b9be47eac90f468475228ba89917a
Guzz <gustavo.bragada@hotmail.com>
2025-02-15 11:16:57 -0300

    finish '12_findTheOldest' exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = Array.from(people).sort((a, b) => {
+        const currentYear = new Date().getFullYear()
+        aLived = a.yearOfDeath ? a.yearOfBirth - a.yearOfDeath : a.yearOfBirth - currentYear;
+        bLived = b.yearOfDeath ? b.yearOfBirth - b.yearOfDeath : b.yearOfBirth - currentYear;
+        return aLived > bLived ? 1 : -1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
