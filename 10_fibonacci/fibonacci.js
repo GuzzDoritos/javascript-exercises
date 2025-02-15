366ae77f5c2f7168d20f5d1406b96c1d1b2803d3
Guzz <gustavo.bragada@hotmail.com>
2025-02-15 10:52:34 -0300

    Finish '10_fibonacci' exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    index = parseInt(member);
+    if (index < 0) return "OOPS";
+    const fib = [0, 1, 1];
+    for (let i = 2; i < index; i++) {
+        next = fib[i] + fib[i - 1];
+        fib.push(next);
+    }
+    return fib[index];
 };
 
 // Do not edit below this line
